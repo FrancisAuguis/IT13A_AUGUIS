44aa705a9317d24dbc508d1d3de01b88bfb7588c
misspatriana <delacruzfrancis502@gmail.com>
2025-02-14 11:48:53 +0800

    Java File
    Added HelloWorld class

--- IT13A_AUGUIS/src/Prelim/Act1_Arithmetic_Operators.java
+++ IT13A_AUGUIS/src/Prelim/Act1_Arithmetic_Operators.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Prelim;
 
-/**
- *
- * @author Administrator
- */
+
 public class Act1_Arithmetic_Operators {
-    
+     public static void main(String[] org){
+             
+        int numb1 = 10;
+        int numb2 = 6;
+        int numb3 = 4;
+        int numb4 = 3; 
+        
+        
+        
+        int AUGUIS1 = numb1 * numb3 + numb2;
+        int UNDO1 = numb1 - numb3;
+        int AUGUIS2 = UNDO1 % numb2;
+        int UNDO2 = numb1 + numb3 + numb2;
+        int AUGUIS3 = UNDO2 / numb4;
+        int UNDO3 = numb3 * numb3;
+        int AUGUIS4 = numb1 * numb2 - UNDO3;
+        
+          System.out.println("10 * 4 + 6 ="+ AUGUIS1);
+         System.out.println("(10-4) % 6 = "+ AUGUIS2);
+            System.out.println("(10 + 4 + 6)/3 = "+ AUGUIS3);
+               System.out.println("10 * 6 -(4 * 4 ) = "+ AUGUIS4);
+               
+     }
 }
